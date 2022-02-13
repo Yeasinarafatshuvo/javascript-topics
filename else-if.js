ee7478ba1d44805a0f0a1957ded1e7ee58784007
Yeasin Arafat <yeasinshuvo76@gmail.com>
2022-02-13 14:57:41 +0600

    Create else-if.js

---
+++ else-if.js
@@ -0,0 +1,10 @@
+let time = 5;
+if (time < 10) {
+    greeting = "Good morning";
+  } else if (time < 20) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }
+
+  console.log(greeting);

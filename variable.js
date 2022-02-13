bc55be588aee5ac81e5cc478abf92488ac91fef0
Yeasin Arafat <yeasinshuvo76@gmail.com>
2022-02-13 12:53:49 +0600

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,22 @@
+/*
+4 Ways to Declare a JavaScript Variable:
+Using var
+->can be re-declared
+->can be re-assigned value
+Using let
+->assign the block scope
+->can not be re-declared
+->can be re-assigned value
+Using const
+->can not be re-declared
+->can not be  re-assigned value
+
+Using nothing
+
+
+*/
+
+var a = 5;
+let b = 6;
+const PI = 3.141516;
+x = 10;

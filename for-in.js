7500728097c5f67e12890c29114c527e1fb482e2
Yeasin Arafat <yeasinshuvo76@gmail.com>
2022-02-13 15:18:50 +0600

    Create for-in.js

---
+++ for-in.js
@@ -0,0 +1,9 @@
+const person = {fname:"John", lname:"Doe", age:25};
+for(let x in person){
+    console.log(person[x]);
+}
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+for(let x in numbers){
+    console.log(numbers[x]);
+}

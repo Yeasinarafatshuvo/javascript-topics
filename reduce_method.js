dbfa90b188efb77b29a3a46f9513d3dc52a1722f
Yeasin Arafat <yeasinshuvo76@gmail.com>
2022-06-26 18:08:22 +0600

    Update reduce_method.js

--- reduce_method.js
+++ reduce_method.js
@@ -1,6 +1,6 @@
 //reduce method does not affect the source array
 var numbers = [1,2,3,4,5,6,7,8,9,10];
-var sum = numbers.reduce((prevValue, currentValue) => {
+var sum = numbers.reduce((prevValue, currentValue,currentIndex, arr) => {
     return prevValue + currentValue;
 }, 0);
 

7390a51913f4a7030a2e68a5cc382e14f77067b8
Yeasin Arafat <yeasinshuvo76@gmail.com>
2023-07-10 10:16:50 +0600

    Update array_find_method.js

--- array_find_method.js
+++ array_find_method.js
@@ -1,4 +1,4 @@
-//find method affect the source array
+//find method does not affect the source array
 var numbers = [1,2,3,4,5,6,7,8,9,10];
 var result = numbers.find(function(currentValue){
     return currentValue > 4;
